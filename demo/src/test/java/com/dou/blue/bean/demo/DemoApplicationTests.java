768d03908442994cbed0265bab8f7527fcfff51e
douspeng <douspeng@sina.cn>
2022-08-10 19:04:12 +0800

    使用gradle platform插件 使用polaris注册中心

---
+++ demo/src/test/java/com/dou/blue/bean/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dou.blue.bean.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
