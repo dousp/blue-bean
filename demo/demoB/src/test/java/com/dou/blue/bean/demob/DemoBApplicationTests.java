98aede5b7362da8599e57a546d93038fe7f9caf3
douspeng <douspeng@sina.cn>
2022-08-16 15:09:05 +0800

    增加演示项目与子项目

---
+++ demo/demoB/src/test/java/com/dou/blue/bean/demob/DemoBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dou.blue.bean.demob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ demo/demoC/src/main/java/com/dou/blue/bean/democ/DemoCApplication.java
@@ -0,0 +1,13 @@
+package com.dou.blue.bean.democ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCApplication.class, args);
+    }
+
+}
